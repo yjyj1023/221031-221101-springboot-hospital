26b9520e5670b7703fd358b85c6a46331a0e8f2a
yjyj1023 <lyj19961023@gmail.com>
2022-11-01 13:26:45 +0900

    add메소드추가 Insert Query에 값 매핑

--- src/main/java/com/example/hello/dao/HospitalDao.java
+++ src/main/java/com/example/hello/dao/HospitalDao.java
@@ -15,6 +15,20 @@ public class HospitalDao {
 
     //List<Hospital> - 11만건의 데이터를 하나씩(Hospital)꺼내서 넣기
     public void add(Hospital hospital){
-        String sql = "INSERT INTO hospitals";
+        String sql = "INSERT INTO `likelion-db`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`) " +
+                "VALUES (?,?,?," +
+                " ?, ?, ?," +
+                " ?, ?, ?," +
+                " ?, ?, ?," +
+                " ?, ?, ?, ?);";
+
+        this.jdbcTemplate.update(sql, hospital.getId(), hospital.getOpenServiceName(),
+                hospital.getOpenLocalGovernmentCode(), hospital.getManagementNumber(),
+                hospital.getLicenseDate(), hospital.getBusinessStatus(),
+                hospital.getBusinessStatusCode(), hospital.getPhone(),
+                hospital.getFullAddress(), hospital.getRoadNameAddress(),
+                hospital.getHospitalName(), hospital.getBusinessTypeName(),
+                hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(),
+                hospital.getTotalNumberOfBeds(), hospital.getTotalAreaSize());
     }
 }
